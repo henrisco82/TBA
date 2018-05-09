b35155a4d41ac65c3a1102acdd55f06e2637c884
Unah Henry <henry.unah@gmail.com>
2018-05-09 04:18:07 +0200

    php starter project completed

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// APP ROOT
+
+ define('APPROOT', dirname(dirname(__FILE__)));
+
+ // URL Root
+ define ('URLROOT', 'http://localhost/mvcapp' );
+
+ // Site Name 
+ define ('SITENAME', 'Pending');
+
+ /**
+ * Configuration for: Database
+ * This is the place where you define your database credentials, database type etc.
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tmvc');
+define('DB_USER', 'root');
+define('DB_PASS', 'jaydenjames');
